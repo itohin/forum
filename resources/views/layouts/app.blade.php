30864ea0590a6d46b336a5623db0934f38c08854
itohin <itoxa@MacBook-Pro.local>
2019-05-18 14:41:42 +0300

    old data

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,13 +36,16 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('threads.index') }}">All Threads</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('threads.create') }}">Create Thread</a>
+                        </li>
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 Channels <span class="caret"></span>
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                @foreach(App\Channel::all() as $channel)
+                                @foreach($channels as $channel)
                                     <a class="dropdown-item" href="{{ route('channels.index', $channel) }}">
                                         {{ $channel->name }}
                                     </a>
