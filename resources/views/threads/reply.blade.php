bfba1d1e5c9c35c242cb7c311727233061ca47e7
itohin <itoxa@MacBook-Pro.local>
2019-05-19 12:51:05 +0300

    favorite button

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,7 +1,19 @@
 <div class="card mb-3">
     <div class="card-header">
-        <a href="#">{{ $reply->owner->name }}</a>
-        said {{ $reply->created_at->diffForHumans() }}
+        <div class="level">
+            <h5 class="flex">
+                <a href="#">{{ $reply->owner->name }}</a>
+                said {{ $reply->created_at->diffForHumans() }}
+            </h5>
+            <div>
+                <form action="{{route('favorites.reply', $reply)}}" method="POST">
+                    @csrf
+                    <button type="submit" class="btn btn-outline-secondary" {{ $reply->isFavorited() ? 'disabled' : '' }}>
+                        {{ $reply->favorites()->count() }} {{ \Illuminate\Support\Str::plural('Favorite', $reply->favorites()->count()) }}
+                    </button>
+                </form>
+            </div>
+        </div>
     </div>
     <div class="card-body">
         {{ $reply->body }}
